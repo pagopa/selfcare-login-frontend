d8fe9fa8b6ccb346973824784ffdedc76d0330e2
MarioDolci9 <mario.dolci@everis.nttdata.com>
2021-10-20 20:40:36 +0200

    fixing login landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import { useEffect } from 'react';
 // import { setCookie, getCookie } from "./utils/cookie";
 import Login from './pages/login/Login';
-
+import { storageWrite, storageRead } from './lib/storage-utils';
 function App() {
-  const token = 'dddd';
-
-  useEffect(() => {}, []);
-
-  if (token) {
+  const { hash = '' } = window.location;
+  const urlToken = hash.replace('#token=', '');
+  if (window.location.href === 'http://localhost:3000/') {
+    console.log(window.location);
     return <Login />;
+  } else {
+    if (window.location.href === `http://localhost:3000/login/success#token=${urlToken}`) {
+      console.log(urlToken);
+      storageWrite('token', urlToken, 'string');
+      const sessionStorageRead = storageRead('token', 'string');
+      console.log(sessionStorageRead);
+      // window.location.assign(`${process.env.REACT_APP_SPID_LOGIN}`);
+    }
   }
+
   return <div></div>;
 }
 
