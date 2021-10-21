b834cc3f0446ab6431e948b4b46f45418c5629ad
MarioDolci9 <mario.dolci@everis.nttdata.com>
2021-10-21 12:20:08 +0200

    login behavior

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
 // import { setCookie, getCookie } from "./utils/cookie";
 import Login from './pages/login/Login';
-import { storageWrite, storageRead } from './lib/storage-utils';
+import { storageWrite, storageRead, storageDelete } from './lib/storage-utils';
+
+const redirectSuccessLogin = () => {
+  window.location.assign(`${process.env.REACT_APP_SPID_DASHBOARD}`);
+};
+const redirectToLogin = () => {
+  window.location.assign(`${process.env.REACT_APP_SPID_LOGIN}`);
+};
+
 function App() {
-  const { hash = '' } = window.location;
-  const urlToken = hash.replace('#token=', '');
-  if (window.location.href === 'http://localhost:3000/') {
-    console.log(window.location);
-    return <Login />;
-  } else {
-    if (window.location.href === `http://localhost:3000/login/success#token=${urlToken}`) {
-      console.log(urlToken);
-      storageWrite('token', urlToken, 'string');
-      const sessionStorageRead = storageRead('token', 'string');
-      console.log(sessionStorageRead);
-      // window.location.assign(`${process.env.REACT_APP_SPID_LOGIN}`);
-    }
+  const token = storageRead('token', 'string');
+  if (window.location.pathname === '/logout') {
+    storageDelete('token');
+    redirectToLogin();
   }
 
-  return <div></div>;
+  if (token !== null && token !== undefined) {
+    redirectSuccessLogin();
+  }
+  switch (window.location.pathname) {
+    case '/login':
+      return <Login />;
+
+    case `/login/success`:
+      const { hash = '' } = window.location;
+      const urlToken = hash.replace('#token=', '');
+
+      if (urlToken !== '' && urlToken !== undefined) {
+        storageWrite('token', urlToken, 'string');
+        // TODO read and store  uid
+        redirectSuccessLogin();
+      } else {
+        redirectToLogin();
+      }
+      return <div></div>;
+    case '/':
+    default:
+      redirectToLogin();
+      return <div></div>;
+  }
 }
 
 export default App;
