642610354e0cb8db0d9854d3b03d8f2e0a06332a
MarioDolci9 <mario.dolci@everis.nttdata.com>
2021-11-19 12:13:23 +0100

    removing theme from files and adding pagoPa theme from npm

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import { registerLocale, setDefaultLocale } from "react-datepicker";
 import { ThemeProvider } from '@mui/material/styles';
-import theme from './theme';
+import theme from '@pagopa/mui-italia/theme';
 import App from './App';
 
 ReactDOM.render(
