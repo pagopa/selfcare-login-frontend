dfd5eea5fdc7137590d084dd7f1f09cfb22c8243
USERSAD\esignore <elena.signore@nttdata.com>
2021-11-24 10:58:00 +0100

    SELC-260 modified index.css with lineHeight and removed App.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 // import { registerLocale, setDefaultLocale } from "react-datepicker";
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '@pagopa/mui-italia/theme';
