c8e89754843bfbf17f6ee7662b5cb2dd69aa1dcc
USERSAD\esignore <elena.signore@nttdata.com>
2022-04-20 11:51:01 +0200

    SELC-859

--- src/locale/it.ts
+++ src/locale/it.ts
@@ -1,16 +1,15 @@
 export default {
   loginPage: {
-    title: 'Accedi con SPID o CIE',
-    description:
-      'Seleziona la modalità di autenticazione che preferisci e inizia il processo di adesione',
+    title: 'Entra con SPID o CIE',
+    description: 'Seleziona la modalità di autenticazione che preferisci.',
     loginBox: {
       title: 'Login',
       spidLogin: 'Entra con SPID',
       cieLogin: 'Entra con CIE',
     },
-    hintText: 'Non hai SPID?<1> Scopri di più</1>',
+    hintText: 'Non hai SPID? <1>Scopri di più</1>',
     privacyAndCondition:
-      'Autenticandoti dichiari di aver letto e compreso l< & > <1>Informativa Privacy</1> e i <1>Termini e condizioni d’uso</1> del Portale Self Care',
+      "Autenticandoti dichiari di aver letto e compreso l'<1>Informativa</1> <2/> <3>Privacy</3> e i <5>Termini e condizioni d’uso</5> dell'Area Riservata.",
   },
   spidSelect: {
     title: 'Scegli il tuo SPID',
