57d7034b628eef17337de4bc9253e8e09c810c36
loremedda <98110186+loremedda@users.noreply.github.com>
2023-04-06 12:33:02 +0200

    PNPG-114 Custom title for PNPG login page
    * PNPG-114 Custom title for PNPG login
    
    * PNPG-114 More generical title in loginPage of PNPG
    
    * fixed little typo
    
    * PNPG-114 Resolved broken unit Test

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -43,6 +43,8 @@ const Login = () => {
 
   const { t } = useTranslation();
 
+  const isPnpg = window.location.hostname?.startsWith('pnpg');
+
   const goCIE = () => {
     storageSpidSelectedOps.write(ENV.SPID_CIE_ENTITY_ID);
     trackEvent(
@@ -98,7 +100,7 @@ const Login = () => {
                 textAlign: 'center',
               }}
             >
-              {fromOnboarding ? t('loginPageFromOnboarding.title') : t('loginPage.title')}
+              {fromOnboarding || isPnpg ? t('loginPageFromOnboarding.title') : t('loginPage.title')}
             </Typography>
           </Grid>
         </Grid>
