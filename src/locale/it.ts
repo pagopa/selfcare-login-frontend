9bd9835c268536231cad60e82c896aecf08e6fe9
esignore <elena.signore@nttdata.com>
2023-05-09 15:18:39 +0200

    SELC-2295 - added product in login page if product is present on url

--- src/locale/it.ts
+++ src/locale/it.ts
@@ -12,7 +12,8 @@ export default {
   },
   loginPageFromOnboarding: {
     title: 'Come vuoi accedere?',
-    description: 'Seleziona la modalità che preferisci e inizia il processo di <1 /> adesione.',
+    description:
+      'Seleziona la modalità che preferisci e inizia il processo di <1 /> adesione per il prodotto {{nomeProdotto}}.',
   },
   spidSelect: {
     title: 'Scegli il tuo SPID',
