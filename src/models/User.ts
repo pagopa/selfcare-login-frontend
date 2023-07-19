1cc706a7636262ca4bd97699cd4fc47a30d050ca
esignore <elena.signore@nttdata.com>
2023-07-19 16:39:46 +0200

    SELC-2715 - replace Ã character

--- src/models/User.ts
+++ src/models/User.ts
@@ -14,8 +14,8 @@ export const userFromJwtToken: (token: string) => User = function (token: string
   return {
     uid: jwtUser.uid,
     taxCode: jwtUser.fiscal_number,
-    name: jwtUser.name,
-    surname: jwtUser.family_name,
+    name: jwtUser.name.replace(/Ã/g, 'à'),
+    surname: jwtUser.family_name.replace(/Ã/g, 'à'),
     email: jwtUser.email,
   };
 };
