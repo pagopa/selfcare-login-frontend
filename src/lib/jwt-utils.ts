63b99fdc402fe093927de70cc652bcd4f2f49b8f
esignore <elena.signore@nttdata.com>
2023-07-19 18:17:34 +0200

    SELC-2715 - semplified function

--- src/lib/jwt-utils.ts
+++ src/lib/jwt-utils.ts
@@ -1,17 +1,14 @@
-function decodeUTF8(binary: string) {
-  const bytes = new Uint8Array(binary.length);
-  // eslint-disable-next-line functional/no-let
-  for (let b = 0; b < bytes.length; ++b) {
-    // eslint-disable-next-line functional/immutable-data
-    bytes[b] = binary.charCodeAt(b);
-  }
-  const decoder = new TextDecoder('utf-8');
-  return decoder.decode(bytes);
-}
-
 export const parseJwt = (token: string) => {
   try {
-    return JSON.parse(decodeUTF8(atob(token.split('.')[1])));
+    return JSON.parse(
+      new TextDecoder().decode(
+        Uint8Array.from(
+          atob(token.split('.')[1])
+            .split('')
+            .map((x) => x.charCodeAt(0))
+        )
+      )
+    );
   } catch (e) {
     return null;
   }
