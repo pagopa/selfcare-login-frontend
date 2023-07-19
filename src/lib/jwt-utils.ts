726aa3b75859c420610f64f41623820d37a43d58
esignore <elena.signore@nttdata.com>
2023-07-19 18:04:31 +0200

    SELC-2715 - decode accented characters

--- src/lib/jwt-utils.ts
+++ src/lib/jwt-utils.ts
@@ -1,6 +1,17 @@
+function decodeUTF8(binary: string) {
+  const bytes = new Uint8Array(binary.length);
+  // eslint-disable-next-line functional/no-let
+  for (let b = 0; b < bytes.length; ++b) {
+    // eslint-disable-next-line functional/immutable-data
+    bytes[b] = binary.charCodeAt(b);
+  }
+  const decoder = new TextDecoder('utf-8');
+  return decoder.decode(bytes);
+}
+
 export const parseJwt = (token: string) => {
   try {
-    return JSON.parse(Buffer.from(token.split('.')[1]).toString());
+    return JSON.parse(decodeUTF8(atob(token.split('.')[1])));
   } catch (e) {
     return null;
   }
