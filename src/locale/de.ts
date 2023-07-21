b3b1d6e6f2106942ab7a171374187f2182c6bc03
loremedda <98110186+loremedda@users.noreply.github.com>
2023-07-21 09:26:38 +0200

    SELC-2582 Added localization languages: english, german, french, slovenian

---
+++ src/locale/de.ts
@@ -0,0 +1,27 @@
+export default {
+  loginPage: {
+    title: 'Zugriff auf Area Riservata',
+    description: 'Der Bereich für die Körperschaften, die die Produkte PagoPA nutzen.',
+    loginBox: {
+      title: 'Log-in',
+      spidLogin: 'Zugriff mit SPID',
+      cieLogin: 'Zugriff mit CIE',
+    },
+    privacyAndCondition:
+      'Wenn du dich anmeldest, akzeptierst du die <1>Allgemeinen Geschäftsbedingungen</1> des Dienstes und <3 /> bestätigst, die <5>Datenschutzerklärung</5> gelesen zu haben',
+  },
+  loginPageFromOnboarding: {
+    title: 'Wie möchtest du zugreifen?',
+    description: 'Wähle deinen bevorzugten Modus aus und starte das <1 /> Beitrittsverfahren.',
+  },
+  spidSelect: {
+    title: 'Wähle deinen SPID-Anbieter',
+    cancelButton: 'Abbrechen',
+    closeButton: 'Abmelden',
+  },
+  loginError: {
+    title: 'Es ist leider ein Fehler aufgetreten.',
+    message:
+      'Aufgrund eines Systemfehlers ist es nicht möglich, den Vorgang abzuschließen.<1 />Bitte versuche es später erneut.',
+  },
+};
---
+++ src/locale/sl.ts
@@ -0,0 +1,26 @@
+export default {
+  loginPage: {
+    title: 'Dostopaj do rezerviranega območja',
+    description: 'Prostor, namenjen subjektom, ki uporabljajo izdelke PagoPA.',
+    loginBox: {
+      title: 'Prijava',
+      spidLogin: 'Dostopaj s SPID',
+      cieLogin: 'Dostopaj s CIE',
+    },
+    privacyAndCondition:
+      'Z dostopom se strinjate s <1>Pogoji in določili uporabe</1> storitve in <3 /> potrjujete, da ste prebrali <5>Izjavo o zasebnosti</5>',
+  },
+  loginPageFromOnboarding: {
+    title: 'Kako želite dostopati?',
+    description: 'Izberite želeni način in začnite postopek <1 /> pristopa.',
+  },
+  spidSelect: {
+    title: 'Izberite svoj SPID',
+    cancelButton: 'Prekliči',
+    closeButton: 'Izhod',
+  },
+  loginError: {
+    title: 'Žal, nekaj je šlo narobe.',
+    message: 'Zaradi sistemske napake postopka ni mogoče dokončati.<1 />Poskusite znova kasneje.',
+  },
+};
