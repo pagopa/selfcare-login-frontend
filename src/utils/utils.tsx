cb907837ceea9b186c7fc02d670cbeaa8d4b3009
Oraldo Doci <oraldo.doci@nttdata.com>
2023-10-18 12:06:51 +0200

    hide documentation button in pnpg

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -3,3 +3,6 @@ import { ROUTE_LOGIN } from './constants';
 export const redirectToLogin = () => {
   window.location.assign(ROUTE_LOGIN);
 };
+
+export const isPnpg =
+window.location.hostname?.startsWith('pnpg') || window.location.hostname?.startsWith('imprese');
