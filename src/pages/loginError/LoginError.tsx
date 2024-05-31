fa3aadb8779db9eab232481fc5c0935382dc2fc0
loremedda <98110186+loremedda@users.noreply.github.com>
2024-05-31 16:10:35 +0200

    [SELC-5006] feat: Managed more frequent SPID authentication errors with specific error pages
    * [SELC-5006] feat: Managed more frequent SPID authentication errors with specific error pages
    
    * temp commit
    
    * temp commit

--- src/pages/loginError/LoginError.tsx
+++ src/pages/loginError/LoginError.tsx
@@ -1,62 +1,140 @@
 import { trackEvent } from '@pagopa/selfcare-common-frontend/services/analyticsService';
 import { Trans, useTranslation } from 'react-i18next';
-import { EndingPage } from '@pagopa/selfcare-common-frontend';
+import { EndingPage, LoadingOverlay } from '@pagopa/selfcare-common-frontend';
 import { IllusError } from '@pagopa/mui-italia';
+import { useEffect, useState } from 'react';
 import { storageSpidSelectedOps } from '../../utils/storage';
-import { ENV } from '../../utils/env';
 import { redirectToLogin } from '../../utils/utils';
-
-const handleError = (errorCode: string | null) => {
-  const spidId = storageSpidSelectedOps.read();
-  trackEvent('LOGIN_FAILURE', { reason: errorCode, SPID_IDP_ID: spidId });
-  console.error(`login unsuccessfull! query params obtained from idp: ${errorCode}`);
-};
 
 const LoginError = () => {
   const { t } = useTranslation();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [title, setTitle] = useState<string | React.ReactElement>();
+  const [description, setDescription] = useState<string | React.ReactElement>();
+  const [haveRetryButton, setHaveRetryButton] = useState<boolean>();
 
   const errorCode = new URLSearchParams(window.location.search).get('errorCode');
 
-  setTimeout(() => redirectToLogin(), 3000);
+  useEffect(() => {
+    if (errorCode) {
+      handleError(errorCode);
+    }
+  }, [errorCode]);
 
-  const title =
-    errorCode === '23'
-      ? t('loginError.errors.suspendedOrRevoked.title')
-      : t('loginError.errors.generic.title');
+  const handleError = (errorCode: string) => {
+    setLoading(true);
 
-  const description =
-    errorCode === '23' ? (
-      <>
-        <Trans
-          i18nKey="loginError.errors.suspendedOrRevoked.description"
-          components={{ 1: <br /> }}
-        >
-          {
-            'La tua identità SPID risulta sospesa o revocata. Per maggiori <1 />informazioni, contatta il tuo fornitore di identità SPID.'
-          }
-        </Trans>
-      </>
-    ) : (
-      <Trans i18nKey="loginError.errors.generic.description" components={{ 1: <br /> }}>
-        {
-          'A causa di un errore del sistema non è possibile completare la procedura.<1 /> Ti chiediamo di riprovare più tardi.'
-        }
-      </Trans>
-    );
-  handleError(errorCode);
-
-  return (
-    <EndingPage
-      icon={errorCode === '23' ? <IllusError size={60} /> : undefined}
-      minHeight={'100vh'}
-      variantTitle="h4"
-      title={title}
-      variantDescription="body1"
-      description={description}
-      buttonLabel={errorCode === '23' ? t('loginError.errors.suspendedOrRevoked.close') : undefined}
-      onButtonClick={
-        errorCode === '23' ? () => window.location.assign(ENV.URL_FE.LANDING) : undefined
+    switch (errorCode) {
+      case '19': {
+        setTitle(
+          <Trans i18nKey="loginError.tooManyAttempts.title" components={{ 1: <br /> }}>
+            {'Hai effettuato troppi tentativi di <1 />accesso'}
+          </Trans>
+        );
+        setDescription(
+          <Trans
+            i18nKey="loginError.tooManyAttempts.description"
+            components={{ 1: <br />, 3: <br /> }}
+          >
+            {
+              'Hai inserito troppe volte un nome utente o password non corretti. <1 />Verifica i dati di accesso e riprova fra qualche minuto, o contatta il <3 />tuo fornitore di identità SPID per modificare le tue credenziali.'
+            }
+          </Trans>
+        );
+        setHaveRetryButton(true);
+        break;
       }
+      case '20': {
+        setTitle(
+          <Trans i18nKey="loginError.incompatibleCredentials.title" components={{ 1: <br /> }}>
+            {'Hai effettuato troppi tentativi di <1 />accesso'}
+          </Trans>
+        );
+        setDescription(
+          <Trans
+            i18nKey="loginError.incompatibleCredentials.description"
+            components={{ 1: <br />, 3: <br /> }}
+          >
+            {
+              'Per motivi di sicurezza, devi utilizzare un’identità con un livello di <1 />sicurezza superiore. Per avere più informazioni, contatta il tuo <3 />fornitore di identità SPID.'
+            }
+          </Trans>
+        );
+        setHaveRetryButton(false);
+        break;
+      }
+      case '21': {
+        setTitle(t('loginError.authTimeout.title'));
+        setDescription(
+          <Trans i18nKey="loginError.authTimeout.description" components={{ 1: <br /> }}>
+            {"È passato troppo tempo da quando hai iniziato l'accesso: riparti <1 />dall'inizio."}
+          </Trans>
+        );
+        setHaveRetryButton(true);
+        break;
+      }
+      case '22': {
+        setTitle(
+          <Trans i18nKey="loginError.deniedByUser.title" components={{ 1: <br /> }}>
+            {'Non hai dato il consenso all’invio <1 />dei dati'}
+          </Trans>
+        );
+        setDescription(t('loginError.deniedByUser.description'));
+        setHaveRetryButton(true);
+        break;
+      }
+      case '23': {
+        setTitle(t('loginError.suspendedOrRevoked.title'));
+        setDescription(
+          <Trans i18nKey="loginError.suspendedOrRevoked.description" components={{ 1: <br /> }}>
+            {
+              'La tua identità SPID risulta sospesa o revocata. Per maggiori <1 />informazioni, contatta il tuo fornitore di identità SPID.'
+            }
+          </Trans>
+        );
+        setHaveRetryButton(false);
+        break;
+      }
+      case '25': {
+        setTitle(t('loginError.canceledbyUser.title'));
+        setDescription(t('loginError.canceledbyUser.description'));
+        setHaveRetryButton(true);
+        break;
+      }
+      default: {
+        setTitle(t('loginError.generic.title'));
+        setDescription(
+          <Trans i18nKey="loginError.generic.description" components={{ 1: <br /> }}>
+            {'Si è verificato un problema durante l’accesso. Riprova tra qualche <1/>minuto.'}
+          </Trans>
+        );
+        setHaveRetryButton(true);
+        break;
+      }
+    }
+
+    const spidId = storageSpidSelectedOps.read();
+    trackEvent('LOGIN_FAILURE', { reason: errorCode, SPID_IDP_ID: spidId });
+    console.error(`login unsuccessfull! error code obtained from idp: ${errorCode}`);
+  };
+
+  return loading ? (
+    <LoadingOverlay />
+  ) : (
+    <EndingPage
+      minHeight={'100vh'}
+      icon={<IllusError size={60} />}
+      variantTitle="h4"
+      variantDescription="body1"
+      title={title}
+      description={description}
+      variantFirstButton={haveRetryButton ? 'outlined' : 'contained'}
+      variantSecondButton="contained"
+      buttonLabel={t('loginError.close')}
+      secondButtonLabel={t('loginError.retry')}
+      onButtonClick={redirectToLogin}
+      onSecondButtonClick={() => history.go(-1)}
+      haveTwoButtons={haveRetryButton}
     />
   );
 };
