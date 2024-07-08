2f843c10fe1119848874102c5f793d863ef33a68
loremedda <lorenzo.medda@nttdata.com>
2024-07-08 14:54:49 +0200

    [SELC-5226] feat: Temp commit

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,7 +5,7 @@ export const BASE_ROUTE = ENV.PUBLIC_URL;
 export const ROUTE_LOGIN = BASE_ROUTE + '/login';
 export const ROUTE_LOGIN_SUCCESS = BASE_ROUTE + '/login/success';
 export const ROUTE_LOGIN_ERROR = BASE_ROUTE + '/login/error';
-export const ROUTE_TERMS_AND_CONDITION = 'https://dev.selfcare.pagopa.it/termini-di-servizio';
+export const ROUTE_TERMS_AND_CONDITION = BASE_ROUTE.replace('/auth', '') + '/termini-di-servizio';
 export const ROUTE_LOGOUT = ENV.URL_FE.LOGOUT;
 
 export const ENABLE_LANDING_REDIRECT = !ENV.URL_FE.LANDING.endsWith('/auth/logout');
