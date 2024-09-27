8702612b024e4c045e58634a315e263d7a35582b
loremedda <lorenzo.medda@nttdata.com>
2024-09-27 10:37:04 +0200

    [PNPG-217] feat: Removed /auth in url for pnpg

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,4 @@
+import { isPnpg } from '../utils/utils';
 import { ENV } from './env';
 
 export const BASE_ROUTE = ENV.PUBLIC_URL;
@@ -5,8 +6,10 @@ export const BASE_ROUTE = ENV.PUBLIC_URL;
 export const ROUTE_LOGIN = BASE_ROUTE + '/login';
 export const ROUTE_LOGIN_SUCCESS = BASE_ROUTE + '/login/success';
 export const ROUTE_LOGIN_ERROR = BASE_ROUTE + '/login/error';
-export const ROUTE_TERMS_AND_CONDITION = BASE_ROUTE + '/termini-di-servizio';
-export const ROUTE_PRIVACY_DISCLAIMER = BASE_ROUTE + '/informativa-privacy';
+export const ROUTE_TERMS_AND_CONDITION =
+  (isPnpg ? BASE_ROUTE.replace('/auth/', '/') : BASE_ROUTE) + '/termini-di-servizio';
+export const ROUTE_PRIVACY_DISCLAIMER =
+  (isPnpg ? BASE_ROUTE.replace('/auth/', '/') : BASE_ROUTE) + '/informativa-privacy';
 export const ROUTE_LOGOUT = ENV.URL_FE.LOGOUT;
 
 export const ENABLE_LANDING_REDIRECT = !ENV.URL_FE.LANDING.endsWith('/auth/logout');
