4d80ca0de3a64d316c76f62604be01a8c9859780
OraldoDoci <117348307+OraldoDoci@users.noreply.github.com>
2025-03-20 12:49:58 +0100

    SELC-6675 generate state and redirect URI

--- src/components/LoadingOverlay.tsx
+++ src/components/LoadingOverlay.tsx
@@ -21,8 +21,6 @@ export function LoadingOverlay({ loadingText }: LoadingOverlayProps) {
           top: '50%',
           left: '50%',
           transform: 'translate(-50%, -50%)',
-          width: 400,
-          bgcolor: 'background.paper',
           boxShadow: 24,
           p: 4,
           textAlign: 'center',
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,4 +1,7 @@
 import { storageOpsBuilder } from '@pagopa/selfcare-common-frontend/lib/utils/storage-utils';
 
 export const storageOnSuccessOps = storageOpsBuilder<string>('LOGIN:onSuccess', 'string', false);
+export const storageStateOps = storageOpsBuilder<string>('StateGenerated', 'string', false);
+export const storageNonceOps = storageOpsBuilder<string>('Nonce', 'string', false);
+export const storageRedirectURIOps = storageOpsBuilder<string>('redirect_uri', 'string', false);
 export const storageSpidSelectedOps = storageOpsBuilder<string>('SPID_SELECTED', 'object', false);
