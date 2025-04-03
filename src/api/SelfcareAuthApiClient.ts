c4a7e1649157e6cccd455d25b24fbf64780e551b
OraldoDoci <117348307+OraldoDoci@users.noreply.github.com>
2025-04-03 11:34:16 +0200

    typo fix

--- src/api/SelfcareAuthApiClient.ts
+++ src/api/SelfcareAuthApiClient.ts
@@ -7,7 +7,7 @@ import { createClient } from './generated/selfcare-auth/client';
 import { OidcExchangeRequest } from './generated/selfcare-auth/OidcExchangeRequest';
 import { OidcExchangeResponse } from './generated/selfcare-auth/OidcExchangeResponse';
 
-const apiCLient = createClient({
+const apiClient = createClient({
   baseUrl: ENV.URL_API.AUTH,
   basePath: '',
   fetchApi: buildFetchApi(ENV.API_TIMEOUT_MS.DASHBOARD),
@@ -17,7 +17,7 @@ export const SelfcareAuthApi = {
   oneIdentityCodeExchange: async (
     oidcExchangeRequest: OidcExchangeRequest
   ): Promise<OidcExchangeResponse> => {
-    const result = await apiCLient.oidcExchange({ body: oidcExchangeRequest });
+    const result = await apiClient.oidcExchange({ body: oidcExchangeRequest });
     return extractResponse(result, 200, () => {});
   },
 };
