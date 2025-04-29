1f94a2aaecb473fcc319472d4d34d1a3c6afb669
OraldoDoci <117348307+OraldoDoci@users.noreply.github.com>
2025-04-29 16:02:33 +0200

    [SELC-6891] feat: remove breadcrumb component  (#373)
    * SELC-6891 remove breadcrumb component from tos and privacy page
    
    * remove margin top

--- src/pages/TermsAndConditionsPage.tsx
+++ src/pages/TermsAndConditionsPage.tsx
@@ -1,12 +1,9 @@
 /* eslint-disable functional/immutable-data */
-import { useEffect, useRef } from 'react';
-import { Breadcrumbs, Button, Grid, Link, Stack } from '@mui/material';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Grid } from '@mui/material';
 import { theme } from '@pagopa/mui-italia';
-import { useTranslation } from 'react-i18next';
+import { useEffect, useRef } from 'react';
 
 export function TermsAndConditionsPage() {
-  const { t } = useTranslation();
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -38,38 +35,15 @@ export function TermsAndConditionsPage() {
     };
   }, []);
 
-  const goBack = () => {
-    history.back();
-  };
-
   return (
     <Grid container xs={12} p={3} bgcolor={theme.palette.background.default}>
-      <Stack p={3}>
-        <Grid item sx={{ display: 'flex', alignItems: 'center' }}>
-          <Button
-            variant="text"
-            color="primary"
-            size="small"
-            startIcon={<ArrowBackIcon />}
-            accessKey="b"
-            onClick={goBack}
-          >
-            {t('breadCrumb.back')}
-          </Button>
-          <Breadcrumbs aria-label="breadcrumb">
-            <Link underline="none" color={theme.palette.text.disabled} sx={{ cursor: 'default' }}>
-              {t('breadCrumb.termsAndConditions')}
-            </Link>
-          </Breadcrumbs>
-        </Grid>
-        <Grid mt={5}>
-          <div
-            id="otnotice-6f92cced-3bd1-4859-9295-baecfc74c64a"
-            className="otnotice"
-            ref={containerRef}
-          ></div>
-        </Grid>
-      </Stack>
+      <Grid>
+        <div
+          id="otnotice-6f92cced-3bd1-4859-9295-baecfc74c64a"
+          className="otnotice"
+          ref={containerRef}
+        ></div>
+      </Grid>
     </Grid>
   );
 }
