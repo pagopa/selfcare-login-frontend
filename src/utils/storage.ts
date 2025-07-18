c355a0205ef43f62e294282eb634f1b0166d3147
OraldoDoci <117348307+OraldoDoci@users.noreply.github.com>
2025-07-18 17:09:44 +0200

    feat: update storage operations to enable persistence for login and state management (#383)
    * feat: update storage operations to enable persistence for login and state management
    
    * clean redirect from local storage

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,8 +1,8 @@
 import { storageOpsBuilder } from '@pagopa/selfcare-common-frontend/lib/utils/storage-utils';
 
-export const storageOnSuccessOps = storageOpsBuilder<string>('LOGIN:onSuccess', 'string', false);
-export const storageStateOps = storageOpsBuilder<string>('StateGenerated', 'string', false);
-export const storageNonceOps = storageOpsBuilder<string>('Nonce', 'string', false);
-export const storageRedirectURIOps = storageOpsBuilder<string>('redirect_uri', 'string', false);
+export const storageOnSuccessOps = storageOpsBuilder<string>('LOGIN:onSuccess', 'string', true);
+export const storageStateOps = storageOpsBuilder<string>('StateGenerated', 'string', true);
+export const storageNonceOps = storageOpsBuilder<string>('Nonce', 'string', true);
+export const storageRedirectURIOps = storageOpsBuilder<string>('redirect_uri', 'string', true);
 export const storageOTPSessionUidOps = storageOpsBuilder<string>('OTPSessionUid', 'string', false);
 export const storageMaskedEmailOps = storageOpsBuilder<string>('MaskedEmail', 'string', false);
