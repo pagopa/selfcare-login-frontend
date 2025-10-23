101e248a86afff3033fdc66b9d17b5f747a751e8
OraldoDoci <117348307+OraldoDoci@users.noreply.github.com>
2025-10-23 11:05:07 +0200

    [SELC-7868] feat: update privacy policy and terms conditions scripts to use new settings and resource URLs (#387)

--- src/pages/TermsAndConditionsPage.tsx
+++ src/pages/TermsAndConditionsPage.tsx
@@ -14,7 +14,10 @@ export function TermsAndConditionsPage() {
     script.charset = 'UTF-8';
     script.id = 'otprivacy-notice-script';
 
-    (script as any).settings = process.env.REACT_APP_OT_TOKEN;
+    script.setAttribute(
+      'settings',
+      'eyJjYWxsYmFja1VybCI6Imh0dHBzOi8vcHJpdmFjeXBvcnRhbC1kZS5vbmV0cnVzdC5jb20vcmVxdWVzdC92MS9wcml2YWN5Tm90aWNlcy9zdGF0cy92aWV3cyIsImNvbnRlbnRBcGlVcmwiOiJodHRwczovL3ByaXZhY3lwb3J0YWwtZGUub25ldHJ1c3QuY29tL3JlcXVlc3QvdjEvZW50ZXJwcmlzZXBvbGljeS9kaWdpdGFscG9saWN5L2NvbnRlbnQiLCJtZXRhZGF0YUFwaVVybCI6Imh0dHBzOi8vcHJpdmFjeXBvcnRhbC1kZS5vbmV0cnVzdC5jb20vcmVxdWVzdC92MS9lbnRlcnByaXNlcG9saWN5L2RpZ2l0YWxwb2xpY3kvbWV0YS1kYXRhIn0='
+    );
 
     document.body.appendChild(script);
 
@@ -22,7 +25,7 @@ export function TermsAndConditionsPage() {
     script.onload = () => {
       (window as any).OneTrust.NoticeApi.Initialized.then(() => {
         (window as any).OneTrust.NoticeApi.LoadNotices([
-          process.env.REACT_APP_OT_TERMS_AND_CONDITION_RESOURCE,
+          'https://privacyportalde-cdn.onetrust.com/storage-container/77f17844-04c3-4969-a11d-462ee77acbe1/privacy-notices/6f92cced-3bd1-4859-9295-baecfc74c64a/published/privacynotice.json',
         ]);
       });
     };
