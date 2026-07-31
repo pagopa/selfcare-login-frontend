7151a85e41a7c7eb085fcd099c0b82f575b10e8c
OraldoDoci <117348307+OraldoDoci@users.noreply.github.com>
2026-07-31 09:30:42 +0200

    [SELC-9115] feat: backstage implement OneTrust notice handling in PrivacyPolicy and TermsAndConditions pages, update dependencies (#423)

--- src/pages/PrivacyPolicyPage.tsx
+++ src/pages/PrivacyPolicyPage.tsx
@@ -2,6 +2,25 @@
 import { Grid } from '@mui/material';
 import { theme } from '@pagopa/mui-italia';
 import { useEffect, useRef } from 'react';
+import { OneTrustNoticeConfig, useOneTrustNotice } from '../hooks/useOneTrustNotice';
+
+const normalConfig: OneTrustNoticeConfig = {
+  noticeId: '26403d01-dc46-4c89-be70-4894839cf639',
+  settings:
+    'eyJjYWxsYmFja1VybCI6Imh0dHBzOi8vcHJpdmFjeXBvcnRhbC1kZS5vbmV0cnVzdC5jb20vcmVxdWVzdC92MS9wcml2YWN5Tm90aWNlcy9zdGF0cy92aWV3cyIsImNvbnRlbnRBcGlVcmwiOiJodHRwczovL3ByaXZhY3lwb3J0YWwtZGUub25ldHJ1c3QuY29tL3JlcXVlc3QvdjEvZW50ZXJwcmlzZXBvbGljeS9kaWdpdGFscG9saWN5L2NvbnRlbnQiLCJtZXRhZGF0YUFwaVVybCI6Imh0dHBzOi8vcHJpdmFjeXBvcnRhbC1kZS5vbmV0cnVzdC5jb20vcmVxdWVzdC92MS9lbnRlcnByaXNlcG9saWN5L2RpZ2l0YWxwb2xpY3kvbWV0YS1kYXRhIn0=',
+  jsonUrl:
+    'https://privacyportalde-cdn.onetrust.com/storage-container/77f17844-04c3-4969-a11d-462ee77acbe1/privacy-notices/26403d01-dc46-4c89-be70-4894839cf639/published/privacynotice.json',
+};
+
+/** OneTrust notice used when the page is reached from a Backstage/PagoPA context
+ * (`origin=backstage` query param appended by `@pagopa/selfcare-common-frontend`). */
+const backstageConfig: OneTrustNoticeConfig = {
+  noticeId: 'f2ca8812-959e-48e9-8eed-61a72d131052',
+  settings:
+    'eyJjYWxsYmFja1VybCI6Imh0dHBzOi8vcHJpdmFjeXBvcnRhbC1kZS5vbmV0cnVzdC5jb20vcmVxdWVzdC92MS9wcml2YWN5Tm90aWNlcy9zdGF0cy92aWV3cyIsImNvbnRlbnRBcGlVcmwiOiJodHRwczovL3ByaXZhY3lwb3J0YWwtZGUub25ldHJ1c3QuY29tL3JlcXVlc3QvdjEvZW50ZXJwcmlzZXBvbGljeS9kaWdpdGFscG9saWN5L2NvbnRlbnQiLCJtZXRhZGF0YUFwaVVybCI6Imh0dHBzOi8vcHJpdmFjeXBvcnRhbC1kZS5vbmV0cnVzdC5jb20vcmVxdWVzdC92MS9lbnRlcnByaXNlcG9saWN5L2RpZ2l0YWxwb2xpY3kvbWV0YS1kYXRhIn0=',
+  jsonUrl:
+    'https://privacyportalde-cdn.onetrust.com/storage-container/77f17844-04c3-4969-a11d-462ee77acbe1/privacy-notices/f2ca8812-959e-48e9-8eed-61a72d131052/published/privacynotice.json',
+};
 
 export function PrivacyPolicyPage() {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -15,46 +34,18 @@ export function PrivacyPolicyPage() {
     }
   }, []);
 
-  useEffect(() => {
-    const script = document.createElement('script');
-
-    script.src = process.env.VITE_OT_SRC ?? '';
-    script.type = 'text/javascript';
-    script.charset = 'UTF-8';
-    script.id = 'otprivacy-notice-script';
-
-    script.setAttribute(
-      'settings',
-      'eyJjYWxsYmFja1VybCI6Imh0dHBzOi8vcHJpdmFjeXBvcnRhbC1kZS5vbmV0cnVzdC5jb20vcmVxdWVzdC92MS9wcml2YWN5Tm90aWNlcy9zdGF0cy92aWV3cyIsImNvbnRlbnRBcGlVcmwiOiJodHRwczovL3ByaXZhY3lwb3J0YWwtZGUub25ldHJ1c3QuY29tL3JlcXVlc3QvdjEvZW50ZXJwcmlzZXBvbGljeS9kaWdpdGFscG9saWN5L2NvbnRlbnQiLCJtZXRhZGF0YUFwaVVybCI6Imh0dHBzOi8vcHJpdmFjeXBvcnRhbC1kZS5vbmV0cnVzdC5jb20vcmVxdWVzdC92MS9lbnRlcnByaXNlcG9saWN5L2RpZ2l0YWxwb2xpY3kvbWV0YS1kYXRhIn0='
-    );
-
-    document.body.appendChild(script);
-
-    // eslint-disable-next-line functional/immutable-data
-    script.onload = () => {
-      (window as any).OneTrust.NoticeApi?.Initialized.then(() => {
-        (window as any).OneTrust.NoticeApi.LoadNotices([
-          'https://privacyportalde-cdn.onetrust.com/storage-container/77f17844-04c3-4969-a11d-462ee77acbe1/privacy-notices/26403d01-dc46-4c89-be70-4894839cf639/published/privacynotice.json',
-        ]);
-      });
-    };
-
-    return () => {
-      document.body.removeChild(script);
-      if (containerRef.current) {
-        containerRef.current.innerHTML = '';
-      }
-    };
-  }, []);
+  const { noticeId, isBackstage } = useOneTrustNotice(containerRef, normalConfig, backstageConfig);
 
   return (
     <Grid container xs={12} p={3} bgcolor={theme.palette.background.default}>
+      {isBackstage && (
+        <Grid xs={12}>
+          {/* Language Drop-down element that will control in which language the Privacy Notice is displayed */}
+          <div className="ot-privacy-notice-language-dropdown-container"></div>
+        </Grid>
+      )}
       <Grid>
-        <div
-          id="otnotice-26403d01-dc46-4c89-be70-4894839cf639"
-          className="otnotice"
-          ref={containerRef}
-        ></div>
+        <div id={`otnotice-${noticeId}`} className="otnotice" ref={containerRef}></div>
       </Grid>
     </Grid>
   );
